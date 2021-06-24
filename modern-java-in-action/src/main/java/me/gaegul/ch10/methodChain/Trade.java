cece81fbce5666f8b862090bbcb6dbe582882830
gaegulgaegul <dla9721@gmail.com>
2021-06-24 22:26:26 +0900

    part3 ch10 정리 중

---
+++ modern-java-in-action/src/main/java/me/gaegul/ch10/methodChain/Trade.java
@@ -0,0 +1,22 @@
+package me.gaegul.ch10.methodChain;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class Trade {
+
+    public enum Type { BUY, SELL }
+
+    private Type type;
+    private Stock stock;
+    private int quantity;
+    private double price;
+
+    public double getValue() {
+        return quantity * price;
+    }
+}
---
+++ modern-java-in-action/src/main/java/me/gaegul/ch10/methodChain/TradeBuilder.java
@@ -0,0 +1,16 @@
+package me.gaegul.ch10.methodChain;
+
+public class TradeBuilder {
+    private final MethodChainingOrderBuilder builder;
+    public final Trade trade = new Trade();
+
+    public TradeBuilder(MethodChainingOrderBuilder builder, Trade.Type type, int quantity) {
+        this.builder = builder;
+        trade.setType(type);
+        trade.setQuantity(quantity);
+    }
+
+    public StockBuilder stock(String symbol) {
+        return new StockBuilder(builder, trade, symbol);
+    }
+}

663e377138a0ce22e25777f69420c4bc07daa1bf
gaegulgaegul <dla9721@gmail.com>
2021-07-14 20:55:22 +0900

    study-blog-20 ch19 내용 정리 완료

--- modern-java-in-action/modern-java-in-action-source/src/main/java/me/gaegul/ch19/PatternMatching.java
+++ modern-java-in-action/modern-java-in-action-source/src/main/java/me/gaegul/ch19/PatternMatching.java
@@ -8,7 +8,83 @@ import java.util.function.Supplier;
 
 public class PatternMatching {
     public static void main(String[] args) {
+        simplify();
 
+        Expr e = new BinOp("+", new Number(5), new BinOp("*", new Number(3), new Number(4)));
+        Integer result = evaluate(e);
+        System.out.println(e + " = " + result);
+    }
+
+    public static void simplify() {
+        TriFunction<String, Expr, Expr, Expr> binOpCase = (opName, left, right) -> {
+            if ("+".equals(opName)) {
+                if (left instanceof Number && ((Number) left).val == 0) {
+                    return right;
+                }
+                if (right instanceof Number && ((Number) right).val == 0) {
+                    return left;
+                }
+            }
+            if ("*".equals(opName)) {
+                if (left instanceof Number && ((Number) left).val == 1) {
+                    return right;
+                }
+                if (right instanceof Number && ((Number) right).val == 1) {
+                    return left;
+                }
+
+            }
+            return new BinOp(opName, left, right);
+        };
+        Function<Integer, Expr> numCase = val -> new Number(val);
+        Supplier<Expr> defaultCase = () -> new Number(0);
+
+        Expr e = new BinOp("+", new Number(5), new Number(0));
+        Expr match = patternMatchExpr(e, binOpCase, numCase, defaultCase);
+        if (match instanceof Number) {
+            System.out.println("Number: " + match);
+        }
+        else if (match instanceof BinOp) {
+            System.out.println("BinOp: " + match);
+        }
+    }
+
+    private static Integer evaluate(Expr e) {
+        Function<Integer, Integer> numCase = val -> val;
+        Supplier<Integer> defaultCase = () -> 0;
+        TriFunction<String, Expr, Expr, Integer> binOpCase = (opName, left, right) -> {
+            if ("+".equals(opName)) {
+                if (left instanceof Number && right instanceof Number) {
+                    return ((Number) left).val + ((Number) right).val;
+                }
+                if (right instanceof Number && left instanceof BinOp) {
+                    return ((Number) right).val + evaluate(left);
+                }
+                if (left instanceof Number && right instanceof BinOp) {
+                    return ((Number) left).val + evaluate(right);
+                }
+                if (left instanceof BinOp && right instanceof BinOp) {
+                    return evaluate(left) + evaluate(right);
+                }
+            }
+            if ("*".equals(opName)) {
+                if (left instanceof Number && right instanceof Number) {
+                    return ((Number) left).val * ((Number) right).val;
+                }
+                if (right instanceof Number && left instanceof BinOp) {
+                    return ((Number) right).val * evaluate(left);
+                }
+                if (left instanceof Number && right instanceof BinOp) {
+                    return ((Number) left).val * evaluate(right);
+                }
+                if (left instanceof BinOp && right instanceof BinOp) {
+                    return evaluate(left) * evaluate(right);
+                }
+            }
+            return defaultCase.get();
+        };
+
+        return patternMatchExpr(e, binOpCase, numCase, defaultCase);
     }
 
     static class Expr {}
@@ -22,7 +98,7 @@ public class PatternMatching {
     @ToString
     @AllArgsConstructor
     static class BinOp extends Expr {
-        String opname;
+        String opName;
         Expr left, right;
     }
 
@@ -34,8 +110,8 @@ public class PatternMatching {
         R apply(S s, T t, U u);
     }
 
-    static <T> T patternMatchExpr(Expr e, TriFunction<String, Expr, Expr, T> binopCase, Function<Integer, T> numCase, Supplier<T> defaultCase) {
-        return (e instanceof BinOp) ? binopCase.apply(((BinOp) e).opname, ((BinOp) e).left, ((BinOp) e).right) :
+    static <T> T patternMatchExpr(Expr e, TriFunction<String, Expr, Expr, T> binOpCase, Function<Integer, T> numCase, Supplier<T> defaultCase) {
+        return (e instanceof BinOp) ? binOpCase.apply(((BinOp) e).opName, ((BinOp) e).left, ((BinOp) e).right) :
                 (e instanceof  Number) ? numCase.apply(((Number) e).val) : defaultCase.get();
     }
 }

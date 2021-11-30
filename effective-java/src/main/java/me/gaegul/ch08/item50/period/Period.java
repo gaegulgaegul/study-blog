cf60fcacb6256f0976bedabbbf04121a9c25de6f
gaegulgaegul <dla9721@gmail.com>
2021-11-30 22:45:47 +0900

    study-blog-126 아이템 50 내용 정리 완료

---
+++ effective-java/src/main/java/me/gaegul/ch08/item50/period/Period.java
@@ -0,0 +1,40 @@
+package me.gaegul.ch08.item50.period;
+
+import java.util.Date;
+
+public final class Period {
+    private final Date start;
+    private final Date end;
+
+    /**
+     * @param start 시작 시각
+     * @param end 종료 시각; 시작 시각보다 뒤여야 한다.
+     * @throws IllegalArgumentException 시작 시각이 종료 시각보다 늦을 때 발생한다.
+     * @throws NullPointerException start나 end가 null이면 발생한다.
+     */
+//    public Period(Date start, Date end) {
+//        if (start.compareTo(end) > 0) {
+//            throw new IllegalArgumentException(start + "가 " + end + "보다 늦다.");
+//        }
+//        this.start = start;
+//        this.end = end;
+//    }
+
+    public Period(Date start, Date end) {
+        this.start = new Date(start.getTime());
+        this.end = new Date(end.getTime());
+
+        if (this.start.compareTo(this.end) > 0) {
+            throw new IllegalArgumentException(start + "가 " + end + "보다 늦다.");
+        }
+    }
+
+
+    public Date getStart() {
+        return new Date(start.getTime());
+    }
+
+    public Date getEnd() {
+        return new Date(end.getTime());
+    }
+}

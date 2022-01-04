a589b513d2ea0bf9dbca0a3e37f3580b1239be5a
gaegulgaegul <dla9721@gmail.com>
2022-01-04 22:15:12 +0900

    study-blog-138 아이템55 내용 정리 완료

---
+++ effective-java/src/main/java/me/gaegul/ch08/item55/Before.java
@@ -0,0 +1,20 @@
+package me.gaegul.ch08.item55;
+
+import java.util.Collection;
+import java.util.Objects;
+
+public class Before {
+
+    public static <E extends Comparable<E>> E max(Collection<E> c) {
+        if (c.isEmpty())
+            throw new IllegalArgumentException("빈 컬렉션");
+
+        E result = null;
+        for (E e : c) {
+            if (result == null || e.compareTo(result) > 0) {
+                result = Objects.requireNonNull(e);
+            }
+        }
+        return result;
+    }
+}
